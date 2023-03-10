5d6dc85452bffd608f5261bc40b692f04d3d502a
Nicolas Nouhi <nnouhi@uclan.ac.uk>
2023-03-11 01:39:21 +0200

    Fixed issue when we were not getting an image for a movie

--- lib/models/app_config.dart
+++ lib/models/app_config.dart
@@ -2,10 +2,12 @@ class AppConfig {
   final String apiKey;
   final String baseApiUrl;
   final String baseImageUrl;
+  final String imageNotFoundUrl;
 
   AppConfig({
     required this.apiKey,
     required this.baseApiUrl,
     required this.baseImageUrl,
+    required this.imageNotFoundUrl,
   });
 }
