782694a143cbcd9ee2a2b02444759001025fcf43
Nicolas Nouhi <nnouhi@uclan.ac.uk>
2023-03-18 20:51:32 +0200

    Modified firebase schema to allow for personalized app preferences

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,26 +1,67 @@
 import 'dart:async';
+// Services
+import '../services/sharedpreferences_service.dart';
 // Packages
+import 'package:get_it/get_it.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:uuid/uuid.dart';
 
 class FirebaseService {
   late DatabaseReference _databaseReference;
   late StreamSubscription _streamSubscription;
+  late String _uuid;
 
   FirebaseService() {
     FirebaseDatabase.instance.setPersistenceEnabled(true);
     _databaseReference = FirebaseDatabase.instance.ref();
     _databaseReference.keepSynced(false);
+
+    _checkIfUserExists();
+  }
+
+  void _checkIfUserExists() async {
+    SharedPreferencesService sp =
+        GetIt.instance.get<SharedPreferencesService>();
+
+    String firebaseUID = await sp.getString('firebaseUID');
+
+    // If user id doesn't exist, create a new one
+    if (firebaseUID == '') {
+      _uuid = Uuid().v4();
+      bool success = await sp.setString('firebaseUID', _uuid);
+      if (success) {
+        _addNewUser();
+      }
+    } else {
+      _uuid = firebaseUID;
+    }
+  }
+
+  void _addNewUser() {
+    _databaseReference.child('users').child(_uuid).set({
+      'app-preferences': {
+        'IsDarkTheme': false,
+        'Language': 'English',
+      },
+    });
   }
 
   // App Language
   void setOnlineAppLanguage(String language, Function appThemeCallback) {
-    _databaseReference.child('app-preferences').child('Language').set(language);
+    _databaseReference
+        .child('users')
+        .child(_uuid)
+        .child('app-preferences')
+        .child('Language')
+        .set(language);
     appThemeCallback();
   }
 
   Future<String> getOnlineAppLanguage() async {
     try {
       DataSnapshot snapshot = await _databaseReference
+          .child('users')
+          .child(_uuid)
           .child('app-preferences')
           .child('Language')
           .get();
@@ -34,6 +75,8 @@ class FirebaseService {
   // App Theme
   void setOnlineAppTheme(bool isDarkTheme, Function appThemeCallback) {
     _databaseReference
+        .child('users')
+        .child(_uuid)
         .child('app-preferences')
         .child('IsDarkTheme')
         .set(isDarkTheme);
@@ -44,6 +87,8 @@ class FirebaseService {
   Future<bool> getOnlineAppTheme() async {
     try {
       DataSnapshot snapshot = await _databaseReference
+          .child('users')
+          .child(_uuid)
           .child('app-preferences')
           .child('IsDarkTheme')
           .get();
